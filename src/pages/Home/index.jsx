cf7328d3971ec28ec19cf8f11a2f601b590eca31
Julian Vazquez <julianvazquez101@gmail.com>
2023-03-13 08:05:31 +0100

    Home page: use fetch status to render components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 import { PodcastAPI } from '../../api/PodcastAPI';
 import { LS_KEYS } from '../../constants';
-import useFetch from '../../hooks/useFetch';
+import useFetch, { STATUS } from '../../hooks/useFetch';
 import PodcastCount from './PodcastCount';
 import PodcastFilter from './PodcastFilter';
 import PodcastList from './PodcastList';
 import * as S from './styles';
 
 const Home = () => {
-    const { data: podcasts = [] } = useFetch({
+    const { data: podcasts = [], status } = useFetch({
         url: PodcastAPI.getTopPodcastsUrl({ limit: 100, genre: 1310 }),
         lsKey: LS_KEYS.TopPodcasts,
     });
     const [filter, setFilter] = useState('');
 
-    const filteredPodcasts =
-        podcasts?.feed?.entry?.filter((p) => {
-            const nameLowerCase = p['im:name']?.label?.toLowerCase();
-            const artistLowerCase = p['im:artist']?.label?.toLowerCase();
-            const filterLowerCase = filter?.toLowerCase();
-            const containsName = nameLowerCase.includes(filterLowerCase);
-            const containsArtist = artistLowerCase.includes(filterLowerCase);
-            return containsName || containsArtist;
-        }) || [];
+    const filteredPodcasts = getFilteredPodcasts(podcasts?.feed?.entry, filter);
+
+    if (status === STATUS.Error) {
+        return <p>Failed to load podcasts.</p>;
+    }
+
+    if (status === STATUS.Idle || status === STATUS.Fetching) {
+        return <p>Loading podcasts...</p>;
+    }
 
     return (
         <>
@@ -35,6 +35,19 @@ const Home = () => {
             </S.FilterContainer>
             <PodcastList podcasts={filteredPodcasts || []} />
         </>
+    );
+};
+
+const getFilteredPodcasts = (podcasts, filter) => {
+    return (
+        podcasts?.filter((p) => {
+            const nameLowerCase = p['im:name']?.label?.toLowerCase();
+            const artistLowerCase = p['im:artist']?.label?.toLowerCase();
+            const filterLowerCase = filter?.toLowerCase();
+            const containsName = nameLowerCase.includes(filterLowerCase);
+            const containsArtist = artistLowerCase.includes(filterLowerCase);
+            return containsName || containsArtist;
+        }) || []
     );
 };
 
